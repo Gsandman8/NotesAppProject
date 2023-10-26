9b2dcbf5d3415e7905f5cf56b68a1ddc9ccbb07e
Gsandman8 <gage.e.sandman@gmail.com>
2023-10-25 23:47:00 -0700

    Added post and delete api routes

--- Develop/routes/api/apiNotes/index.js
+++ Develop/routes/api/apiNotes/index.js
@@ -1,15 +1,32 @@
 const router = require('express').Router();
-const notes = require('../../../db/db.json');
 const fs = require('fs');
 const path = require('path');
+const {v4:uuid} = require('uuid');
 
 router.get("/", (req, res) => {
     const parsedNotes = JSON.parse(fs.readFileSync(path.resolve(__dirname + '../../../../db/db.json')));
     res.json(parsedNotes);
 });
+
 router.post("/", (req, res) => {
-    console.log(req.body);
-    console.log(notes);
+    const newNote = {
+        id: uuid(),
+        title: req.body.title,
+        text: req.body.text
+    };
+
+    const noteV1 = JSON.parse(fs.readFileSync(path.resolve(__dirname + "../../../../db/db.json")));
+    noteV1.push(newNote);
+    fs.writeFileSync(path.resolve(__dirname + "../../../../db/db.json"), JSON.stringify(noteV1));
+    res.json(noteV1);
+});
+
+router.delete("/:id", (req, res) => {
+    const noteId = req.params.id;
+    const noteV1 = JSON.parse(fs.readFileSync(path.resolve(__dirname + "../../../../db/db.json")));
+    const noteV2 = noteV1.filter(note => note.id !== noteId);
+    fs.writeFileSync(path.resolve(__dirname +  "../../../../db/db.json"), JSON.stringify(noteV2));
+    res.json(noteV2);
 });
 
 module.exports = router;
